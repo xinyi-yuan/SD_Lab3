647a88142b1e228f9d807fa7d457d243f423344e
Xinyi Yuan <xinyi-yuan@uiowa.edu>
2023-11-11 16:38:58 -0600

    basic home page look complete, need tabs

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, flash, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = "0000"
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run()
