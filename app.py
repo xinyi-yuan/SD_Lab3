e66d56ac921292c2a66d011d8d2a827c4d8b2dba
Xinyi Yuan <xinyi-yuan@uiowa.edu>
2023-11-13 14:08:06 -0600

    Fixed not redirecting to the correct pages (url_for())

--- app.py
+++ app.py
@@ -9,5 +9,40 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/xinyi")
+def xinyi():
+    return render_template("xinyi.html")
+
+
+@app.route("/hannah")
+def hannah():
+    return render_template("hannah.html")
+
+
+@app.route("/will")
+def will():
+    return render_template("will.html")
+
+
+@app.route("/nathan")
+def nathan():
+    return render_template("nathan.html")
+
+
+@app.route("/project")
+def project():
+    return render_template("project.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run()
