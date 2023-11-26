6c2f810f7ab731dfac5f63bada0fafc4e65f6272
Xinyi Yuan <xinyi-yuan@uiowa.edu>
2023-11-25 19:24:49 -0600

    Login and signup are implemented!

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL is None:
     raise ValueError("No DATABASE_URL set for Flask application")
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cur = conn.cursor()
 
 @app.route("/")
 def home():
@@ -45,13 +44,53 @@ def project():
     return render_template("project.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['pword']
+
+        cur = conn.cursor()
+        cur.execute("SELECT email, password FROM users WHERE email = %s", (email,))
+        user = cur.fetchone()
+        cur.close()
+
+        # Check if user exists and the password is correct
+        if user and check_password_hash(user[1], password):
+            flash("Login Successful!")
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid email or password, try again')
+            return redirect(url_for('login'))
     return render_template("login.html")
 
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['pword']
+        confirm_pword = request.form['pword2']
+        user_name = request.form['name']
+
+        if password != confirm_pword:
+            flash('Passwords do no match!')
+            return redirect(url_for('signup'))
+
+        hashed_password = generate_password_hash(password)
+
+        try:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO users (email, password, user_name) VALUES (%s, %s, %s)",
+            (email, hashed_password, user_name))
+            conn.commit()
+            cur.close()
+            flash("Account created successfully!")
+            return redirect(url_for('login'))
+        except Exception as e:
+            conn.rollback()
+            flash("Error: " + str(e))
+            return redirect(url_for('sign_up'))
     return render_template("signup.html")
 
 @app.route("/forgot")
